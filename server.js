f07f60d91e41284434f7b58f9124f2bc54966a11
jmartinez <jcmartinez1925@gmail.com>
2019-10-17 21:16:53 -0700

    set up server and dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mpngoose");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
+});
