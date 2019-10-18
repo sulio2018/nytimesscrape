9a6c9aafb915f9770922c2f842aabc09edfdfdfb
jmartinez <jcmartinez1925@gmail.com>
2019-10-17 22:23:47 -0700

    set up models

---
+++ models/Headline.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const headlineSchema = new Schema ({
+    
+    headline: {
+        type: String,
+        required: true,
+        unique: { index: { unique: true } }
+    },
+
+    summary: {
+        type: String,
+        required: true
+    },
+
+    url: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Headline = mongoose.model("Headline", headlineSchema);
+
+module.exports = Headline;
